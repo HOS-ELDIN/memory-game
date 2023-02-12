f9ca90889af6bb8c5b9cf499f4befa548e1b85b8
HOSSAM-ELDIN <HOS-ELDIN@users.noreply.github.com>
2023-02-12 14:50:57 +0200

    great edit

--- main.js
+++ main.js
@@ -6,17 +6,13 @@ let boxs = document.querySelectorAll(".box");
 let game = document.querySelector("section");
 let result = document.querySelector(".result");
 let playAgain = document.querySelector(".result button");
+let StartAnimation = flippingGen();
 
 // get user name from prompt
 start.onclick = () => {
   startWindow.style.display = "none";
   userName.innerHTML = startInput.value;
-  setTimeout(() => {
-    game.classList.add("congrats");
-    setTimeout(() => {
-      game.classList.remove("congrats");
-    }, 2400);
-  }, 600);
+  StartAnimation.next();
 };
 
 //#####################################################
@@ -82,14 +78,11 @@ window.addEventListener("click", (e) => {
 //#####################################################
 //knowing if the game is done
 window.addEventListener("click", (e) => {
-  if (goodTries == 10) {
-    game.classList.add("congrats");
-    setTimeout(() => {
-      game.classList.remove("congrats");
-    }, 4800);
+  if (goodTries == 10 || false) {
+    rotating(4800,false)
     setTimeout(() => {
       result.classList.add("show");
-    }, 5000);
+    }, 5200);
   }
 });
 //#####################################################
@@ -97,3 +90,65 @@ window.addEventListener("click", (e) => {
 playAgain.onclick = () => {
   window.location.reload();
 };
+//#####################################################
+// generator to flip one by one
+function* flippingGen() {
+  // let array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+  let i = 0;
+  while (i < 20) {
+    yield flipOnebyOne(boxs[i]);
+    i++;
+  }
+  yield rotating(2400,true);
+
+  yield returnAll();
+}
+// function to start generator
+function flipOnebyOne(e) {
+  e.classList.add("flipped");
+  setTimeout(() => {
+    StartAnimation.next();
+  }, 300);
+}
+// function to make rotating move
+function rotating(time,next) {
+  game.classList.add("congrats");
+  setTimeout(() => {
+    game.classList.remove("congrats");
+    if (next == true) {
+      StartAnimation.next()
+    }
+  },time);
+}
+// function to return all
+function returnAll() {
+  setTimeout(() => {
+    boxs.forEach((e) => {
+      e.classList.remove("flipped");
+    });
+  }, 600);
+}
+
+
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
+// console.log(flipp.next());
